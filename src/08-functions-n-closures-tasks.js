dd7fd493159fac27811bfe1b08ddef9bd1ecd327
Artem Pasko <guramibox777@gmail.com>
2022-11-25 16:45:35 +0300

    feat: add getIdGeneratorFunction in 08-functions-n-closures-tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -180,8 +180,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let value = startFrom;
+  return () => {
+    const count = value;
+    value += 1;
+    return count;
+  };
 }
 
 
