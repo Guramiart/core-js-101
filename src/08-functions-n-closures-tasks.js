fb531d0f317553ad4085f3b006f9705425292095
Artem Pasko <guramibox777@gmail.com>
2022-11-25 15:36:20 +0300

    feat: add 3 tasks solution in 08-functions-n-closures-tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 
@@ -62,8 +62,19 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return (x) => {
+    let argSize = args.length;
+    if (argSize === 0) {
+      return null;
+    }
+    let y = 0;
+    while (argSize > 0) {
+      y += args[args.length - argSize] * x ** (argSize - 1);
+      argSize -= 1;
+    }
+    return y;
+  };
 }
 
 
